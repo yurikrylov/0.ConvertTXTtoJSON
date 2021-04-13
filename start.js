dfe298acee8b29cfbe7aff1733f61303c578f1ad
krylov <krilov@iicavers.ru>
2021-04-13 15:48:53 +0300

    added json example

--- start.js
+++ start.js
@@ -1,28 +1,49 @@
-let {readFile, readdir} = require('fs');
+let fs = require('fs');
+const fsPromises = fs.promises;
 
+async function readDir() {
+    let response = [];
+    try {
+        response = await fsPromises.readdir('/opt/my_projects/d3').then((value => {
+            return value
+        }));
+    } catch (err) {
+        console.error('Error occurred while reading directory!', err);
+    }
+    return response;
+}
 
-async function readTextFiles(path) {
-    await readdir(path, (err, files) => {
-        if (err) return err
-        return files
-    });
+async function processFiles(files) {
+    for await (const file of files) {
+        let parsedLines = [];
+        const text = await readTextFile(file);
+        const parsedText = await toLines(text);
+        parsedText.map((i) => {
+           parsedLines.push(parseLine(i));
+        })
+       createJSONFile(parsedLines);
+    }
 }
 
 async function readTextFile(path) {
     let options = {};
     let result = [];
-    options.season = path.slice(0,4);
-    options.episode = path.slice(4,6);
-    await readFile(path, 'utf8', (err, data) => {
+    options.season = path.slice(0, 4);
+    options.episode = path.slice(4, 6);
+    await fs.readFile(path, 'utf8', (err, data) => {
         if (err) {
             return err
         }
         const lines = data.split(/\r?\n/);
         lines.map((i) => {
-            result.push(parseLine(i,options));
+            result.push(parseLine(i, options));
         })
         createJSONFile(result);
     })
+}
+
+function toLines(text) {
+    return text.split(/\r?\n/);
 }
 
 function parseLine(line, options) {
@@ -41,14 +62,14 @@ function parseLine(line, options) {
 //  haveNote: '',
 //  note: ''
 // }
-    JSONLine.season=options.season;
-    JSONLine.episode=options.episode;
+    JSONLine.season = options.season;
+    JSONLine.episode = options.episode;
     const personages = ['Monica', 'Rachel', 'Joey', 'Ross', 'Chandler', 'Phoebe'];
     //TODO вся логика здесь
     const lineArr = line.split(' ');
-/*    1. Проверяем на "["  и "("  - сцена или комментарий
-    2. Ищем двоеточие - имя персонажа
-*/
+    /*    1. Проверяем на "["  и "("  - сцена или комментарий
+        2. Ищем двоеточие - имя персонажа
+    */
     //Commercial Break
 
     //scene
@@ -62,24 +83,13 @@ function createJSONFile(result) {
     return result.stringify()
 }
 
-function start() {
-    let files = readTextFiles('myTXTFiles');
-    forEach(files).then(r => console.log('end'));
-}
-function toLines(text){
-     let a =  text.split(/\r?\n/);
-     return a;
-}
-async function forEach(files){
-
-    for await (const file of files) {
-         let parsedLines = [];
-         const text = await readTextFile(file);
-         const parsedText = await toLines(text);
-         parsedText.map((i) => {
-             i |> parseLine |> parsedLines.push;
-        })
-        createJSONFile(parsedLines);
+async function start() {
+    let files = await readDir().then((value) => {
+        return value
+    });
+    if (Array.isArray(files)) {
+        processFiles(files).then((value=>{}, onerror));
     }
 }
+
 start()
