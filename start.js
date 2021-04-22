ab63b493323b84e3288108adedb3d567ceb2ed37
krylov <krilov@iicavers.ru>
2021-04-22 16:25:30 +0300

    added eslintrc

--- start.js
+++ start.js
@@ -1,29 +1,30 @@
 const fs = require('fs');
-const fsPromises = fs.promises;
 const createJSONFile = require('./lib/createJSONFile');
 const parseLine = require('./lib/parseLine');
 const readHTMLFile = require('./lib/readHTMLFile');
-let app = {};
 
-app.options={
-    path:'./'
+const fsPromises = fs.promises;
+const app = {};
+
+app.options = {
+  path: './',
 };
-
-app.start = function() {
-    let fileNames;
-    try {
-        fileNames =  fsPromises.readdir(app.options.path);
-    }
-    catch(err){
-        console.error(err);
-    }
-    fileNames.forEach((name)=>{
-        let lines = readHTMLFile.readFile(name);
-        let oLines = lines.map((line)=>{
-           return parseLine.parseLine(line,app.options);
-        });
-        createJSONFile.create(oLines);
-    })
+async function getFiles() {
+  try {
+    return await fsPromises.readdir(app.options.path);
+  } catch (e) {
+    return e;
+  }
 }
-app.start()
 
+app.start = function start() {
+  // eslint-disable-next-line no-console
+  const fileNames = getFiles().then((value) => value).catch((err) => console.log(err));
+
+  fileNames.forEach((name) => {
+    const lines = readHTMLFile.readFile(name);
+    const oLines = lines.map((line) => parseLine.parseLine(line, app.options));
+    createJSONFile.create(oLines);
+  });
+};
+app.start();
