2ea3657eee616226d20f8b7d2a6634bd2fbb82e8
krylov <krilov@iicavers.ru>
2021-04-26 17:33:35 +0300

    path resolved

--- start.js
+++ start.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const path = require('path');
 const createJSONFile = require('./lib/createJSONFile');
 const parseLine = require('./lib/parseLine');
 const readHTMLFile = require('./lib/readHTMLFile');
@@ -10,9 +11,9 @@ app.options = {
   dir: './txt',
   path: './txt/example',
 };
-async function getFiles() {
+function getFiles() {
   try {
-    return await fsPromises.readdir(app.options.path);
+    return fsPromises.readdir(app.options.path);
   } catch (e) {
     return e;
   }
@@ -21,19 +22,20 @@ async function startAsync() {
   // eslint-disable-next-line no-console
   const fileNames = await getFiles().then((value) => value).catch((err) => console.log(err));
 
-  async function mapForEach(files, create()) {
+  async function mapForEach(files, create) {
     for (let i = 0; i < fileNames.length; i++) {
-      await create(fileNames[i]);
+      create(fileNames[i]);
     }
   }
-  function create(name) {
-    const lines = readHTMLFile.readFile(`${app.optipons.dir}${name}`).catch((err) => console.log(err));
+
+  await mapForEach(fileNames, (name) => {
+    const lines = readHTMLFile.readFile(path.resolve(`${app.options.path}`, `${name}`)).catch((err) => console.log(err));
     const oLines = lines.map((line) => parseLine.parseLine(line, app.options));
     createJSONFile.create(oLines);
-  }
-  await mapForEach(fileNames);
+  });
 }
 app.start = function start() {
   startAsync().catch((err) => console.log(err));
 };
+
 app.start();
