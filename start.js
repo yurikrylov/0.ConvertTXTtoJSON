6b662cb6dd7ee0e23fa56b4956fb5a097c50d46f
Krylov <yuri.krylov@gmail.com>
2021-05-03 18:34:57 +0300

    data changed

--- start.js
+++ start.js
@@ -4,9 +4,17 @@ const { htmlToText } = require('html-to-text');
 
 const fsPromises = fs.promises;
 const parseLine = require('./lib/parseLine');
-const createJSONFile = require('./lib/createJSONFile');
 
 const app = {};
+let JSONDB;
+
+const createJSONFile = {};
+
+const addToJSONDB = function create(lines) {
+  const data = JSON.parse(JSONDB);
+  data.push(lines);
+  JSONDB = JSON.stringify(data);
+};
 
 app.options = {
   dir: './txt',
@@ -22,7 +30,6 @@ function getFiles() {
 }
 
 async function readFile(pathToFile) {
-
   const data = await fsPromises.readFile(pathToFile, 'utf-8');
   const text = await htmlToText(data, { wordwrap: null }).split(/[\r\n]+/);
   return text;
@@ -30,20 +37,26 @@ async function readFile(pathToFile) {
 
 async function wrapper(name) {
   const lines = await readFile(path.resolve(`${app.options.path}`, `${name}`));
-  const oLines = await lines.map((line) => parseLine.parseLine(line, app.options));
-  createJSONFile.create(oLines);
+  const opts = {};
+  opts.season = name.slice(0, 2);
+  opts.episode = name.slice(2, 2);
+  const oLines = await lines.map((line) => parseLine.parseLine(line, opts));
+  return oLines;
 }
 
 async function startAsync() {
+  const newFileName = createJSONFile.create();
   // eslint-disable-next-line no-console
   const fileNames = await getFiles().then((value) => value).catch((err) => console.log(err));
-
+  // eslint-disable-next-line
   for await (const name of fileNames) {
-    return wrapper(name);
+    const oLines = wrapper(name);
+    addToJSONDB(newFileName, oLines);
   }
 }
 
 app.start = function start() {
+  // eslint-disable-next-line no-console
   startAsync().catch((err) => console.log(err));
 };
 
